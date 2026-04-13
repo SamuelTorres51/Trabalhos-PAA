8005f51a2e6efc19f620a43e461828a88d7a7f83
SamuelTorres51 <samuel.torres28@hotmail.com>
2026-04-13 16:39:55 -0300

    adicionando algoritmo do mergesort em py

--- Trabalho01/src/mergesort.py
+++ Trabalho01/src/mergesort.py
@@ -1,2 +1,26 @@
+def _intercalar(esquerda, direita):
+    resultado = []
+    indice_esquerda = 0
+    indice_direita = 0
+
+    while indice_esquerda < len(esquerda) and indice_direita < len(direita):
+        if esquerda[indice_esquerda] <= direita[indice_direita]:
+            resultado.append(esquerda[indice_esquerda])
+            indice_esquerda += 1
+        else:
+            resultado.append(direita[indice_direita])
+            indice_direita += 1
+
+    resultado.extend(esquerda[indice_esquerda:])
+    resultado.extend(direita[indice_direita:])
+    return resultado
+
+
 def mergesort(arr):
-    raise NotImplementedError("Implementacao pendente")
+    if len(arr) <= 1:
+        return arr[:]
+
+    meio = len(arr) // 2
+    metade_esquerda = mergesort(arr[:meio])
+    metade_direita = mergesort(arr[meio:])
+    return _intercalar(metade_esquerda, metade_direita)
